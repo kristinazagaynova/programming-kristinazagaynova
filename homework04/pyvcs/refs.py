fe27172146a93a9844390521c102926d36c5228c
kristinazagaynova <71030427+kristinazagaynova@users.noreply.github.com>
2021-05-14 16:53:49 +0500

    Add files via upload

---
+++ homework04/pyvcs/refs.py
@@ -0,0 +1,46 @@
+import pathlib
+import typing as tp
+
+
+def update_ref(gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value: str) -> None:
+    ref_file = gitdir / ref
+    with ref_file.open("w") as s:
+        s.write(new_value)
+
+
+def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
+    gitdr = pathlib.Path(gitdir).absolute()
+    pathlib.Path(gitdr/name).touch()
+    file = pathlib.Path(gitdr/name)
+    with open(file, "w") as f:
+        f.write(f"ref: {ref}")
+        f.close()
+
+
+def ref_resolve(gitdir: pathlib.Path, refname: str) -> tp.Optional[str]:
+    if refname == "HEAD" and not is_detached(gitdir):
+        return resolve_head(gitdir)
+    if (gitdir / refname).exists():
+        with (gitdir / refname).open() as f:
+            return f.read().strip()
+    return None
+
+
+def resolve_head(gitdir: pathlib.Path) -> tp.Optional[str]:
+    with (gitdir / "HEAD").open() as f:
+        return ref_resolve(gitdir, get_ref(gitdir))
+
+
+def is_detached(gitdir: pathlib.Path) -> bool:
+    if get_ref(gitdir) == "":
+        return True
+    return False
+
+
+def get_ref(gitdir: pathlib.Path) -> str:
+    with (gitdir / "HEAD").open() as f:
+        data = f.read().strip().split()
+        if len(data) == 2:
+            return data[1]
+        else:
+            return ""
